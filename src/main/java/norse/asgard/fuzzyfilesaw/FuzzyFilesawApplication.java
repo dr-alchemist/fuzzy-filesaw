c0403c9fe79271314e32110e3a7fdb9d300b8d5c
mjolnir <49009913+mjolnir-asgard@users.noreply.github.com>
2020-04-10 16:04:29 -0700

    init...

---
+++ src/main/java/norse/asgard/fuzzyfilesaw/FuzzyFilesawApplication.java
@@ -0,0 +1,13 @@
+package norse.asgard.fuzzyfilesaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzyFilesawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuzzyFilesawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/norse/asgard/fuzzyfilesaw/FuzzyFilesawApplicationTests.java
@@ -0,0 +1,13 @@
+package norse.asgard.fuzzyfilesaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzzyFilesawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
